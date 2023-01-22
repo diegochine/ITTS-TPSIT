4c75ae1f458e0dc763debbf4df4047645d98f08e
Diego Chinellato <chine.diego@gmail.com>
2023-01-22 22:58:58 +0100

    added gui, fixed philosophers-good.py

--- concurrency/philosophers-bad.py
+++ concurrency/philosophers-bad.py
@@ -1,39 +1,68 @@
 from threading import Thread, Lock
+import tkinter as tk
+from tkinter import ttk
 import random
 import time
 
 
 class Philosopher(Thread):
 
-    def __init__(self, index, left_fork, right_fork):
+    def __init__(self, index, left_fork, right_fork, label):
         super(Philosopher, self).__init__()
         self.index = index
         self.left_fork = left_fork
         self.right_fork = right_fork
+        self.label = label
 
     def run(self):
-        for i in range(10):
-            print(f'Philosopher {self.index} is hungry.')
-            self.dine()
-            time.sleep(5)
+        for _ in range(10):
+            self.pickup_forks()
+            self.eat()
+            self.putdown_forks()
+            self.think()
 
-    def dine(self):
+    def pickup_forks(self):
         self.left_fork.acquire()
-        self.right_fork.acquire()
+        # time.sleep(0.00001) # remove comment to cause deadlock
+        locked = self.right_fork.acquire(False)
+        if not locked:
+            self.label['text'] = 'HUNGRY [LEFT FORK]'
+            self.right_fork.acquire()
 
-        print(f'Philosopher {self.index} starts eating.')
-        time.sleep(5)
-        print(f'Philosopher {self.index} finishes eating and leaves to think.')
-
+    def putdown_forks(self):
         self.left_fork.release()
         self.right_fork.release()
 
+    def think(self):
+        self.label['text'] = 'THINKING'
+        time.sleep(random.randint(3, 10))
+        self.label['text'] = 'HUNGRY'
+
+    def eat(self):
+        self.label['text'] = 'EATING'
+        time.sleep(random.randint(3, 10))
+
 
 if __name__ == "__main__":
-    locks = [Lock() for n in range(5)]
+    root = tk.Tk()
+    root.geometry('600x300')
+    root.title('Dining philosophers')
+    root.grid()
 
-    philosophers = [Philosopher(i, locks[i % 5], locks[(i + 1) % 5])
-                    for i in range(5)]
+    forks = [Lock() for n in range(5)]
+    philosophers = []
+
+    for i in range(5):
+        name_label = ttk.Label(root, text=f'Philosopher {i}', font=("Colibri", 22))
+        name_label.grid(column=0, row=i * 2, columnspan=2)
+        ttk.Label(root, text='\t').grid(column=2, row=i * 2, columnspan=2)
+
+        status_label = ttk.Label(root, text=f'HUNGRY', font=("Colibri", 22))
+        status_label.grid(column=4, row=i * 2, columnspan=3)
+
+        philosopher = Philosopher(i, forks[i % 5], forks[(i + 1) % 5], status_label)
+        philosophers.append(philosopher)
 
     for p in philosophers: p.start()
-    for p in philosophers: p.join()
+
+    root.mainloop()
--- concurrency/philosophers-good.py
+++ concurrency/philosophers-good.py
@@ -1,48 +1,79 @@
 from threading import Thread, Lock
+import tkinter as tk
+from tkinter import ttk
 import random
 import time
 
 
 class Philosopher(Thread):
 
-    def __init__(self, index, left_fork, right_fork, semaphore):
+    def __init__(self, index, left_fork, right_fork, mutex, label):
         super(Philosopher, self).__init__()
         self.index = index
         self.left_fork = left_fork
         self.right_fork = right_fork
-        self.semaphore = semaphore
+        self.mutex = mutex
+        self.label = label
 
     def run(self):
-        for i in range(10):
-            print(f'Philosopher {self.index} is hungry.')
-            self.dine()
-            time.sleep(5)
+        for _ in range(5):
+            self.pickup_forks()
+            self.eat()
+            self.putdown_forks()
+            self.think()
 
     def pickup_forks(self):
-        self.left_fork.acquire()
-        self.right_fork.acquire()
+        has_forks = False
+
+        while not has_forks:
+            self.mutex.acquire()
+
+            has_left = self.left_fork.acquire(blocking=False)
+            has_right = self.right_fork.acquire(blocking=False)
+            if has_left and has_right:
+                has_forks = True
+            elif has_left:
+                self.left_fork.release()
+            elif has_right:
+                self.right_fork.release()
+
+            self.mutex.release()
 
     def putdown_forks(self):
         self.left_fork.release()
         self.right_fork.release()
 
-    def dine(self):
-        with self.semaphore:
-            self.pickup_forks()
+    def think(self):
+        self.label['text'] = 'THINKING'
+        time.sleep(random.randint(3, 10))
+        self.label['text'] = 'HUNGRY'
 
-        print(f'Philosopher {self.index} starts eating.')
-        time.sleep(5)
-        print(f'Philosopher {self.index} finishes eating and leaves to think.')
-
-        self.putdown_forks()
+    def eat(self):
+        self.label['text'] = 'EATING'
+        time.sleep(random.randint(3, 10))
 
 
 if __name__ == "__main__":
-    forks = [Lock() for n in range(5)]
-    semaphore = Lock()
+    root = tk.Tk()
+    root.geometry('600x300')
+    root.title('Dining philosophers')
+    root.grid()
 
-    philosophers = [Philosopher(i, forks[i % 5], forks[(i + 1) % 5], semaphore)
-                    for i in range(5)]
+    forks = [Lock() for n in range(5)]
+    mutex = Lock()
+    philosophers = []
+
+    for i in range(5):
+        name_label = ttk.Label(root, text=f'Philosopher {i}', font=("Colibri", 22))
+        name_label.grid(column=0, row=i * 2, columnspan=2)
+        ttk.Label(root, text='\t').grid(column=2, row=i * 2, columnspan=2)
+
+        status_label = ttk.Label(root, text=f'HUNGRY', font=("Colibri", 22))
+        status_label.grid(column=4, row=i * 2, columnspan=3)
+
+        philosopher = Philosopher(i, forks[i % 5], forks[(i + 1) % 5], mutex, status_label)
+        philosophers.append(philosopher)
 
     for p in philosophers: p.start()
-    for p in philosophers: p.join()
+
+    root.mainloop()
